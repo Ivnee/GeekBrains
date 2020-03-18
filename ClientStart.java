9a188615b518a6af91ebb943dfed7bd75c4de7ce
Ivan Ognev <Ognev-dmitrov@mail.ru>
2020-03-18 15:26:59 +0300

    hw6

--- ClientStart.java
+++ ClientStart.java
@@ -1,9 +1,7 @@
-package hw6;
+package Data;
 
 public class ClientStart {
     public static void main(String[] args) {
-
         Client client = new Client();
-
     }
 }
--- Server.java
+++ Server.java
@@ -1,50 +1,38 @@
-package hw6;
+package Data;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class Server {
     public static void main(String[] args) {
-        ServerSocket server;
-        Socket socket;
-        Scanner sc;
-        PrintWriter out;
         try {
-            server = new ServerSocket(8080);
-            System.out.println("Сервер включен ");
-            socket = server.accept();
-            sc = new Scanner(socket.getInputStream());
-            System.out.println("Пользователь подключился ");
-            out = new PrintWriter(socket.getOutputStream());
-            new Thread(() -> {
-                while(true){
-                    System.out.println("Введите сообщение:");
-                    String str = sc.nextLine();
-                    System.out.println("Сервер: " + str);
-                    out.println("Сервер: " + str);
-                    if(str.equals("/end")){
-                        try {
-                            out.println("Сервер отключился");
-                            server.close();
-                            socket.close();
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                        break;
-                    }
-                }
-            }).start();
+            ServerSocket server = new ServerSocket(8181);
+            System.out.println("Сервер включился");
+            Socket socket = server.accept();
+            System.out.println("Пользователь подключился");
+            Scanner sc = new Scanner(System.in);
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            DataInputStream in = new DataInputStream(socket.getInputStream());
 
-        } catch (IOException e) {
+            new Thread(()-> {
+                    try {
+                        while(true){
+                        String text = sc.nextLine();
+                        String msg = in.readUTF();
+                        out.writeUTF("Пользователь: "+ msg);
+                        out.writeUTF("Сервер: " + text);
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+            }).start();
+            } catch (IOException e) {
             e.printStackTrace();
         }
 
-
     }
-
 }
